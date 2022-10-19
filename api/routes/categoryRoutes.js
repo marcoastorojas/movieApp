34d8af09faec902e8363a5e942f89cb7729d825b
Marco Asto <marcoasto123@gmail.com>
2022-10-19 12:52:43 -0500

    get movies and series by category

--- api/routes/categoryRoutes.js
+++ api/routes/categoryRoutes.js
@@ -2,6 +2,8 @@ const { Router } = require("express")
 const { body } = require("express-validator")
 const { validateResult } = require("../helpers/validateResults")
 const Category = require("../models/Category")
+const Movie = require("../models/Movie")
+const Serie = require("../models/Serie")
 
 const categoryRouter = Router()
 categoryRouter.post("/", [body('name').exists(), validateResult], async (req, res) => {
@@ -11,6 +13,14 @@ categoryRouter.post("/", [body('name').exists(), validateResult], async (req, re
 categoryRouter.get("/", async (req, res) => {
     const categories = await Category.findAll()
     res.status(200).json({ ok: true, results: categories })
+})
+categoryRouter.get("/:id/movies", async (req, res) => {
+    const category = await Category.findByPk(req.params.id,{include:Movie})
+    res.status(200).json({ ok: true, results: category.movies })
+})
+categoryRouter.get("/:id/serie", async (req, res) => {
+    const category = await Category.findByPk(req.params.id,{include:Serie})
+    res.status(200).json({ ok: true, results: category.series })
 })
 // categoryRouter.post("/")
 // categoryRouter.put("/:id")
